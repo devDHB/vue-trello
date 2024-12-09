ed68729891e96e12407847bd74f839b2e8222488
bang <myownlife323@naver.com>
2024-12-09 17:48:26 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'app',
+  components : {Navbar},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
