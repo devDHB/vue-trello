e48ec085c95d15aaec0caa59b1545862820db0bf
doo B <myownlife323@naver.com>
2024-12-11 03:26:51 +0900

    Board.vue edit

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,65 +1,35 @@
 <template>
-  <Modal>
-    <div slot="header">
-      <h2>
-        Create new board
-        <a
-          href=""
-          class="modal-default-button"
-          @click.prevent="SET_IS_ADD_BOARD(false)"
-          >&times;</a
-        >
-      </h2>
+  <div>
+    Board
+    <div v-if="loading">loading board...</div>
+    <div v-else>
+      <div>bid: {{ bid }}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+      <hr />
+      <router-view></router-view>
     </div>
-    <div slot="body">
-      <form id="add-board-form" @submit.prevent="addBoard">
-        <input class="form-control" type="text" v-model="input" ref="input" />
-      </form>
-    </div>
-    <div slot="footer">
-      <button
-        class="btn"
-        :class="{ 'btn-success': valid }"
-        type="submit"
-        form="add-board-form"
-        :disabled="!valid"
-      >
-        Create Board
-      </button>
-    </div>
-  </Modal>
+  </div>
 </template>
 
 <script>
-import { mapMutations, mapActions } from "vuex";
-import Modal from "./Modal.vue";
-
 export default {
-  components: {
-    Modal,
-  },
   data() {
     return {
-      input: "",
-      valid: false,
+      bid: 0,
+      loading: false,
     };
   },
-  watch: {
-    input(v) {
-      this.valid = v.trim().length > 0;
-    },
-  },
-  mounted() {
-    this.$refs.input.focus();
+  created() {
+    this.fetchData();
   },
   methods: {
-    ...mapMutations(["SET_IS_ADD_BOARD"]),
-    ...mapActions(["ADD_BOARD", "FETCH_BOARDS"]),
-    addBoard() {
-      this.SET_IS_ADD_BOARD(false);
-      this.ADD_BOARD({ title: this.input }).then((_) => {
-        this.FETCH_BOARDS();
-      });
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.bid = this.$route.params.bid;
+        this.loading = false;
+      }, 500);
     },
   },
 };
